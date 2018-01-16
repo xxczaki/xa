851a289bb1f4f0e7e0f018863fff7bdb230b55da
Anoni Kepinski <keppi@o2.pl>
2018-01-16 21:56:36 +0100

    Commenting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 
-const colorMap = {
+const colorMap = {  //Define colors
 	info: 'green',
 	warn: 'yellow',
 	loading: 'blue',
@@ -8,16 +8,16 @@ const colorMap = {
 	error: 'red'
 };
 
-const print = (type, label, message) => {
+const print = (type, label, message) => {  
 	const color = colorMap[type];
 	console.log(
-    chalk.inverse.bold[color](` ${label} `),
+    chalk.inverse.bold[color](` ${label} `), //Background color
     chalk[color](message),
     '\n'
   );
 };
 
-const info = (label, message) => print('info', label, message);
+const info = (label, message) => print('info', label, message); 
 const warn = (label, message) => print('warn', label, message);
 const loading = (label, message) => print('loading', label, message);
 const experimental = (label, message) => print('experimental', label, message);
@@ -28,14 +28,14 @@ const link = url => chalk.cyan(url);
 const error = (err, options) => {
 	const label = 'ERROR';
 	if (!options) {
-		options = {exit: true, silent: false, label};
+		options = {exit: true, silent: false, label}; //Error options
 	}
 	print('error', options.label || label, err);
 	if (options.silent) {
-		process.exit(1);
+		process.exit(1); //Error option - silent
 	}
 	if (options.exit) {
-		throw new Error(err);
+		throw new Error(err); //Error option - exit
 	}
 };
 
