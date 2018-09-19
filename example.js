4ff247b1e7067648b3c514802e82b2626bbb869d
Antoni Kepinski <keppi@o2.pl>
2018-09-19 23:09:58 +0200

    Small changes 🚀

--- example.js
+++ example.js
@@ -1,8 +1,10 @@
-const xa = require('.'); // You can run 'node example.js' in the project directory to see the output in your terminal!
+// TIP: You can run 'node example.js' in the project directory to see the output in your terminal!
+
+const xa = require('.');
 
 xa.loading('TEST', 'Running test...');
 
-xa.info('SUCCESS', `Finished converting file!`);
+xa.info('SUCCESS', 'Finished converting file!');
 
 xa.warn('WARN', `This module is deprecated! Read more: ${xa.link('http://example.com')}`);
 
@@ -12,5 +14,4 @@ xa.on('ON', 'You turned on unicorn mode!');
 
 xa.off('OFF', 'You turned off unicorn mode!');
 
-xa.error('Could not find chalk. Make sure you have installed it!', {exit: false});
-
+xa.error('Could not find chalk. Make sure you have installed it!', {silent: true});
--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 const chalk = require('chalk');
 
-const colorMap = { // Define colors
-	info: 'green',
-	warn: 'yellow',
-	loading: 'blue',
+// Color map
+const colorMap = {
+	error: 'red',
 	experimental: 'magenta',
-	on: 'white',
+	info: 'green',
+	loading: 'blue',
 	off: 'gray',
-	error: 'red'
+	on: 'white',
+	warn: 'yellow'
 };
 
+// Background color & text
 const print = (type, label, message) => {
 	const color = colorMap[type];
 	console.log(
-		chalk.inverse.bold[color](` ${label} `), // Background color
+		chalk.inverse.bold[color](` ${label} `),
 		chalk[color](message),
 		'\n'
 	);
 };
 
+// Log types
 const info = (label, message) => print('info', label, message);
 const warn = (label, message) => print('warn', label, message);
 const loading = (label, message) => print('loading', label, message);
 const on = (label, message) => print('on', label, message);
 const off = (label, message) => print('off', label, message);
 const experimental = (label, message) => print('experimental', label, message);
-
-const command = cmd => chalk.cyan(cmd);
-const link = url => chalk.cyan(url);
-
 const error = (err, options) => {
 	const label = 'ERROR';
+	// Loud or silent error (default is loud)
 	if (!options) {
-		options = {exit: true, silent: false, label}; // Error options
+		options = {loud: true, silent: false, label};
 	}
 	print('error', options.label || label, err);
 	if (options.silent) {
-		process.exit(1); // Error option - silent
+		process.exit(1);
 	}
-	if (options.exit) {
-		throw new Error(err); // Error option - exit
+	if (options.loud) {
+		throw new Error(err);
 	}
 };
+
+// Additional log types (command & url)
+const command = cmd => chalk.cyan(cmd);
+const link = url => chalk.cyan(url);
 
 module.exports = {info, loading, warn, experimental, on, off, error, command, link};
