84f75de6c80801bbdadb3b9f1fb0bd23559c2833
Rekseto <ReksetoAjx@gmail.com>
2018-10-23 23:39:39 +0200

    Change examples

--- example.js
+++ example.js
@@ -1,17 +1,34 @@
 // TIP: You can run 'node example.js' in the project directory to see the output in your terminal!
 
-const xa = require('.');
+const { Logger, stdoutHandler } = require(".");
 
-xa.loading('TEST', 'Running test...');
+const xa = Logger();
 
-xa.info('SUCCESS', 'Finished converting file!');
+xa.loading("TEST", "Running test...");
 
-xa.warn('WARN', `This module is deprecated! Read more: ${xa.link('http://example.com')}`);
+xa.info("SUCCESS", "Finished converting file!");
 
-xa.experimental('EXPERIMENTAL', 'This feature is not implemented yet!');
+xa.warn(
+	"WARN",
+	`This module is deprecated! Read more: ${xa.link("http://example.com")}`
+);
 
-xa.on('ON', 'You turned on unicorn mode!');
+xa.experimental("EXPERIMENTAL", "This feature is not implemented yet!");
 
-xa.off('OFF', 'You turned off unicorn mode!');
+xa.on("ON", "You turned on unicorn mode!");
 
-xa.error('Could not find chalk. Make sure you have installed it!', {silent: true});
+xa.off("OFF", "You turned off unicorn mode!");
+
+xa.error("Could not find chalk. Make sure you have installed it!");
+
+const customizedLogger = Logger({
+	handlers: [
+		stdoutHandler({
+			info: "magenta",
+			error: "blue"
+		})
+	]
+});
+
+customizedLogger.info("INFO", "I'm customized stdoutHandler!");
+customizedLogger.error("Something went wrong", "it does not look good...");
