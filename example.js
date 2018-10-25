f82c4ca77b1d7d4d9dd35ba6bb9324cecd4e28e4
Jorge Madson <filholouco@gmail.com>
2018-10-25 18:07:11 -0300

    Removing some undefineds
     I could not fix the undefined of 
    	`This module is deprecated! Read more: ${xa.link('http://example.com','example.com')}`
    because the ${} part is showed before the phrase,and  i did not understand why.

--- example.js
+++ example.js
@@ -10,7 +10,7 @@ xa.info('SUCCESS', 'Finished converting file!');
 
 xa.warn(
 	'WARN',
-	`This module is deprecated! Read more: ${xa.link('http://example.com')}`
+	`This module is deprecated! Read more: ${xa.link('http://example.com','example.com')}`
 );
 
 xa.experimental('EXPERIMENTAL', 'This feature is not implemented yet!');
@@ -19,7 +19,7 @@ xa.on('ON', 'You turned on unicorn mode!');
 
 xa.off('OFF', 'You turned off unicorn mode!');
 
-xa.error('Could not find chalk. Make sure you have installed it!');
+xa.error('Could not find chalk.', 'Make sure you have installed it!');
 
 const customizedLogger = logger({
 	handlers: [
