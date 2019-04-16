edd655fcde83e2f299d369ee4b2265f3f367ceec
Antoni Kepinski <xxczaki@pm.me>
2019-04-16 19:39:07 +0200

    [2.0] Add new examples

--- example.js
+++ example.js
@@ -1,34 +1,13 @@
-// TIP: You can run 'node example.js' in the project directory to see the output in your terminal!
+const xa = require('.');
 
-const {logger, stdoutHandler} = require('.');
+console.log('\n');
 
-const xa = logger();
+xa.success('Build finished!\n');
 
-xa.loading('TEST', 'Running test...');
+xa.info('Loading...\n');
 
-xa.info('SUCCESS', 'Finished converting file!');
+xa.warning('Something is not ok\n');
 
-xa.warn(
-	'WARN',
-	`This module is deprecated! Read more: ${xa.link('http://example.com', 'example.com')}`
-);
+xa.error('Build broken, exiting\n');
 
-xa.experimental('EXPERIMENTAL', 'This feature is not implemented yet!');
-
-xa.on('ON', 'You turned on unicorn mode!');
-
-xa.off('OFF', 'You turned off unicorn mode!');
-
-xa.error('Could not find chalk.', 'Make sure you have installed it!');
-
-const customizedLogger = logger({
-	handlers: [
-		stdoutHandler({
-			info: 'magenta',
-			error: 'blue'
-		})
-	]
-});
-
-customizedLogger.info('INFO', 'Customized stdoutHandler!');
-customizedLogger.error('Something went wrong', 'it does not look good...');
+xa.custom('TITLE', 'Nice description', {titleColor: 'yellow', backgroundColor: '#212121'});
