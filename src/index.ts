055149723d544fdcf923d0061c8bdb3d436dc444
Richie Bendall <richiebendall@gmail.com>
2020-04-01 06:16:20 +1300

    Simplify implementation
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -9,71 +9,7 @@ interface Options {
 	backgroundColor?: string;
 }
 
-exports.success = (text: string) => {
-	if (typeof window !== 'undefined') {
-		console.log(' %cSUCCESS', `%c${text}`, 'background: green; border-radius: 2px');
-	}
-
-	if (is.main()) {
-		console.log(` ${chalk.bgGreen.black(' MAIN ')} ${text}`);
-	}
-
-	if (is.renderer()) {
-		console.log(` ${chalk.bgGreen.black(' RENDERER ')} ${text}`);
-	}
-
-	console.log(` ${chalk.bgGreen.black(' SUCCESS ')} ${text}`);
-};
-
-exports.info = (text: string) => {
-	if (typeof window !== 'undefined') {
-		console.log(' %cINFO', `%c${text}`, 'background: blue; border-radius: 2px');
-	}
-
-	if (is.main()) {
-		console.log(` ${chalk.bgBlue(' MAIN ')} ${text}`);
-	}
-
-	if (is.renderer()) {
-		console.log(` ${chalk.bgBlue(' RENDERER ')} ${text}`);
-	}
-
-	console.log(` ${chalk.bgBlue(' INFO ')} ${text}`);
-};
-
-exports.warning = (text: string) => {
-	if (typeof window !== 'undefined') {
-		console.log(' %cWARNING', `%c${text}`, 'background: yellow; border-radius: 2px');
-	}
-
-	if (is.main()) {
-		console.log(` ${chalk.bgYellow.black(' MAIN ')} ${text}`);
-	}
-
-	if (is.renderer()) {
-		console.log(` ${chalk.bgYellow.black(' RENDERER ')} ${text}`);
-	}
-
-	console.log(` ${chalk.bgYellow.black(' WARNING ')} ${text}`);
-};
-
-exports.error = (text: string) => {
-	if (typeof window !== 'undefined') {
-		console.log(' %cERROR', `%c${text}`, 'background: red; border-radius: 2px');
-	}
-
-	if (is.main()) {
-		console.log(` ${chalk.bgRed(' MAIN ')} ${text}`);
-	}
-
-	if (is.renderer()) {
-		console.log(` ${chalk.bgRed(' RENDERER ')} ${text}`);
-	}
-
-	console.log(` ${chalk.bgRed(' ERROR ')} ${text}`);
-};
-
-exports.custom = (title: string, text: string, {titleColor, backgroundColor}: Options) => {
+const custom = (title: string, text: string, {titleColor, backgroundColor}: Options): void => {
 	const color = Color(titleColor).hex();
 	const bgColor = Color(backgroundColor).hex();
 
@@ -90,4 +26,15 @@ exports.custom = (title: string, text: string, {titleColor, backgroundColor}: Op
 	}
 
 	console.log(` ${chalk.bgHex(bgColor).hex(color)(` ${title} `)} ${text}`);
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const factory = (title: string, backgroundColor: string) => (text: string): void => custom(title, text, {backgroundColor});
+
+export = {
+	success: factory('SUCCESS', 'green'),
+	info: factory('INFO', 'blue'),
+	warning: factory('WARNING', 'yellow'),
+	error: factory('ERROR', 'red'),
+	custom
 };
